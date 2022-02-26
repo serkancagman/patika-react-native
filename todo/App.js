5749c5e212766916a078b29777d7e337c1d6fbdf
serkancagman <serkancagmann@gmail.com>
2022-02-26 17:29:37 +0300

    todoApp

---
+++ todo/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet,SafeAreaView } from 'react-native';
+import TodoBody from './src/components/Todobody/TodoBody';
+
+const App = () => {
+ 
+
+  return (
+    <SafeAreaView style={styles.container}>
+     <TodoBody/>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor:"#DAF7A6",
+  height:"100%",
+position:"relative"}
+});
+
+export default App;
